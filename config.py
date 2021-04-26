a17f569ee5e307a90c103840d8b4bdde6d2f8dc2
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-04-26 07:59:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
 
+# Get a bot token from botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+# Get from my.telegram.org (or @UseTGXBot)
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+# Get from my.telegram.org (or @UseTGXBot)
+API_HASH = os.environ.get("API_HASH", "")
+
+
+# You Can Add More Vars If You Want
